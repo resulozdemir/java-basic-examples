952574a0e137466f90e33086ac255afee3696ffc
Resul Ozdemir <zemirsd9@gmail.com>
2022-10-17 21:19:27 +0300

    dizidekiElemanlariSiralama

---
+++ dizidekiElemanlariSiralama.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class dizidekiElemanlariSiralama {
+    public static void main(String[] args){
+        Scanner inp = new Scanner(System.in);
+
+        int diziBoyutu = 0,sayac,gecici=0,sayac2;
+
+        System.out.print("Dizinin boyutunu giriniz : ");
+        diziBoyutu = inp.nextInt();
+
+        int[] dizi = new int[diziBoyutu];
+
+        for(sayac = 0;sayac < diziBoyutu;sayac++){
+            System.out.print((sayac+1) + ". Elemani girin : ");
+            dizi[sayac] = inp.nextInt();
+        }
+
+        for(sayac = 0;sayac < diziBoyutu - 1;sayac++){
+            for(sayac2 = sayac + 1 ; sayac2 < diziBoyutu ;sayac2++){
+                if(dizi[sayac2] < dizi[sayac]){
+                    gecici = dizi[sayac];
+                    dizi[sayac] = dizi[sayac2];
+                    dizi[sayac2] = gecici;
+                }
+            }
+        }
+
+        for(sayac = 0;sayac < diziBoyutu;sayac++){
+            System.out.println((sayac+1) + ". Elemani : " + dizi[sayac]);
+        }
+
+    }
+}
