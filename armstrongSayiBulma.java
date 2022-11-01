2b9a131a8657f4de991044b742eff093d58ab907
Resul Ozdemir <zemirsd9@gmail.com>
2022-11-01 21:38:43 +0300

    armstrongSayiBulma

---
+++ armstrongSayiBulma.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class armstrongSayiBulma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen bir sayi giriniz: ");
+        String numberAsString = scanner.next();
+
+        int sum = 0;
+
+        for(int i = 0; i < numberAsString.length(); i++){
+            sum += Integer.parseInt(String.valueOf(numberAsString.charAt(i)));
+        }
+        System.out.println("Girilen " + numberAsString + " sayısının rakamlarının toplamı: " + sum);
+    }
+}
