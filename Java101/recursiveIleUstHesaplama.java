530bf8fd57f9634871c5c8d8d2902bc3f8d5bfab
Resul Ozdemir <zemirsd9@gmail.com>
2022-12-01 01:25:03 +0300

    recursiveIleUstHesaplama

---
+++ Java101/recursiveIleUstHesaplama.java
@@ -0,0 +1,26 @@
+package Java101;
+import java.util.Scanner;
+
+public class recursiveIleUstHesaplama {
+    static double hesapla(int taban, int us) {
+        if (us > 0) {
+            return taban * hesapla(taban, us - 1);
+        }
+        else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int taban, us;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Taban değerini giriniz :");
+        taban = inp.nextInt();
+
+        System.out.print("Üs değerini giriniz :");
+        us = inp.nextInt();
+
+        System.out.println(hesapla(taban, us));
+    }
+}
