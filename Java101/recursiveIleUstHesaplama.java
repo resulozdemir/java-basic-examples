3c8fee784516bb55a34a7b9c603d4442a98d7c7d
Resul Ozdemir <zemirsd9@gmail.com>
2022-12-01 01:51:26 +0300

    recursiveIleUstHesaplama

--- Java101/recursiveIleUstHesaplama.java
+++ Java101/recursiveIleUstHesaplama.java
@@ -2,27 +2,28 @@ package Java101;
 import java.util.Scanner;
 
 public class recursiveIleUstHesaplama {
-    static int sayac=0;
-    static int asalSayiMi(int sayi,int i) {
-
-        if (sayi % i != 0 && i < sayi) {
-            return asalSayiMi(sayi,i+1);
+    static double hesapla(int taban, int us) {
+        if (us > 0) {
+            return taban * hesapla(taban, us - 1);
         }
-        else if (sayi % i == 0 && i < sayi){
-            sayac++;
+        else {
+            return 1;
         }
-
-        return sayac;
     }
 
     public static void main(String[] args) {
-        int sayi,i=2;
+        int taban, us;
         Scanner inp = new Scanner(System.in);
 
-        System.out.print("Sayi giriniz :");
-        sayi = inp.nextInt();
+        System.out.print("Taban değerini giriniz :");
+        taban = inp.nextInt();
 
-        String sonuc = asalSayiMi(sayi,i) == 0 ? "Asal sayi" : "Asal sayi degil";
-        System.out.println(sonuc);
+        System.out.print("Üs değerini giriniz :");
+        us = inp.nextInt();
+
+        System.out.println(hesapla(taban, us));
+
+
+
     }
 }
